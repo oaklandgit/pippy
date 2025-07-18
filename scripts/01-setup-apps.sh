0f24842b5dfd33c53a727240132cf02554f72f83
Larry Stone <lazzance@gmail.com>
2025-07-18 10:23:17 -0700

    Update 01-setup-apps.sh

--- scripts/01-setup-apps.sh
+++ scripts/01-setup-apps.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "Uninstalling unused apps..."
 
+# Even though Chromium is installed on Raspberry Pi OS by default,
+# it doesn't actually work on the Pi Zero.
+# So we're replacing it with Midori.
+
 echo "Removing Chromium..."
 sudo apt remove --purge -y chromium-browser chromium
 sudo apt autoremove -y
