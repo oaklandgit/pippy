1a8a73e1d8dd63d4f6aab35bfc836b396fa6371b
oaklandgit <muppethead@mac.com>
2025-07-24 14:30:00 -0700

    Compile pipsqueak into an executable that accepts a file path as an argument.

---
+++ pipsqueak/examples/hello.lua
@@ -0,0 +1,8 @@
+draw_text("Hello, Pippy!", 20, 30)
+draw_text("Testing 1, 2, 3.", 30, 40)
+draw_rect(10, 50, 100, 40, "red")
+
+
+for i=1, 5 do
+  draw_rect(10, i, 100, 40, "red")
+end
--- pipsqueak/scripts/hello.lua
+++
@@ -1,3 +0,0 @@
-draw_text("Hello, Pippy!", 20, 30)
-draw_rect(10, 50, 100, 40, "red")
-
