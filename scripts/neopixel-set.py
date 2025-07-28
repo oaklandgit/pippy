414d25b227c843720e190aa0c0199578c61ce9cb
Larry Stone <lazzance@gmail.com>
2025-07-27 19:27:54 -0700

    Updated neopixel utility to accept 0-7 for specifying the pixel.

--- scripts/neopixel-set.py
+++ scripts/neopixel-set.py
@@ -5,10 +5,28 @@ import board
 import neopixel
 import sys
 
-n = 8 # number of pixels
-pixels = neopixel.NeoPixel(board.D18, n, brightness=0.5, auto_write=False)
+NUM_PIXELS = 8 # number of pixels
+pixels = neopixel.NeoPixel(board.D18, NUM_PIXELS, brightness=0.5, auto_write=False)
 
-color = tuple(map(int, sys.argv[1:4])) # expects RGB
-for i in range(n):
-    pixels[i] = color
+# if 3 args, set all pixels
+if len(sys.argv) == 4:
+    r, g, b = map(int, sys.argv[1:4])
+    for i in range(NUM_PIXELS):
+        pixels[i] = (r, g, b)
+
+# if 4 args, set a single pixel
+elif len(sys.argv) == 5:
+    idx = int(sys.argv[1])
+    r, g, b = map(int, sys.argv[2:5])
+    if 0 <= idx < NUM_PIXELS:
+        pixels[idx] = (r, g, b)
+    else:
+        print(f"Pixel index must be 0-{NUM_PIXELS - 1}")
+        sys.exit(1)
+else:
+    print("Usage:")
+    print("  neopixel-set.py R G B        # set all")
+    print("  neopixel-set.py IDX R G B    # set one")
+    sys.exit(1)
+
 pixels.show()
